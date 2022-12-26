ef173caa05289707b454e4023165000c59b7938e
Kim gimin <ggm4954@gmail.com>
2022-12-26 22:06:09 +0900

    third

--- app.js
+++ app.js
@@ -3,6 +3,25 @@
 const express = require("express")
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+const users = [
+    {"name":"김기민", "level":"student", "phone":"01011112222", "point":50},
+    {"name":"A", "level":"A", "phone":"A", "point":10}
+]
+
+app.post('/signup', (req,res) => {
+    users.push(req.body)
+    console.log(users) //이걸 작성하지 않으면 터미널에 추가된 배열이 보여지지 않음
+    res.send('회원가입 페이지')
+})
+
+
+app.post('/signin', (req,res) => {
+    res.send('로그인 페이지')
+})
 
 app.get('/', (req,res) => {
     res.send('글 리스트')
@@ -23,6 +42,11 @@ app.post("/modify", (req,res) => {
 app.post("/delete", (req,res) => {
     res.send('글 삭제 페이지')
 })
+
+
+
+
+
 
 
 
